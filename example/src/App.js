5087abf51b72a0da2dd7e4b86bc1812fa4c158b6
Herman Starikov <hermanstarikov@gmail.com>
2019-02-01 17:33:45 -0500

    update site

--- example/src/App.js
+++ example/src/App.js
@@ -8,6 +8,11 @@ import * as animations from 'react-animations';
 const animation = keyframes`${animations.fadeIn}`;
 
 const StyledSection = styled.section`
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   height: 101vh;
   background: hsl(${({hue=0}) => hue}, 50%, 50%);
   ${({revealed}) => revealed && css`animation: 1s ${animation}`}
@@ -19,12 +24,41 @@ const MySection = (props) => {
   return <StyledSection revealed={revealed} ref={ref} {...props}/>
 }
 
+const MyA = styled.a`
+color: white;
+margin: 0.5em;
+`
+
+
+const example = `
+// example with styled-components and react-animations
+const StyledSection = styled.section\`
+  // ... your styles
+  \${({revealed}) => revealed && css\`animation: 1s \${animation}\`}
+\`;
+const MySection = (props) => {
+  const ref = useRef()
+  const revealed = useMyHook(ref)
+  return <StyledSection revealed={revealed} ref={ref} {...props}/>
+}`
+
 const App = () => {
   return (
     <div>
-      <MySection/>
-      <MySection hue={120} />
-      <MySection hue={240}/>
+      <MySection>
+
+        <pre><code>{`npm install use-scroll-to-reveal`}</code></pre>
+        <pre><code>{`yarn add use-scroll-to-reveal`}</code></pre>
+
+
+      </MySection>
+      <MySection hue={120}>
+        <pre><code>{example}</code></pre>
+      </MySection>
+      <MySection hue={240}>
+        <MyA href="https://github.com/Hermanya/use-scroll-to-reveal">github</MyA>
+        <MyA href="https://www.npmjs.com/package/use-scroll-to-reveal">npm</MyA>
+      </MySection>
     </div>
   )
 }
