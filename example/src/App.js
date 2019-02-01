d6c520af95c434fb48d8bf66962a8f4e22179c49
Herman Starikov <hermanstarikov@gmail.com>
2019-02-01 17:22:46 -0500

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {useRef} from 'react'
 
 import useMyHook from 'use-scroll-to-reveal'
 
+import styled, { keyframes, css } from 'styled-components';
+import * as animations from 'react-animations';
+
+const animation = keyframes`${animations.fadeIn}`;
+
+const StyledSection = styled.section`
+  height: 101vh;
+  background: hsl(${({hue=0}) => hue}, 50%, 50%);
+  ${({revealed}) => revealed && css`animation: 1s ${animation}`}
+`;
+
+const MySection = (props) => {
+  const ref = useRef()
+  const revealed = useMyHook(ref)
+  return <StyledSection revealed={revealed} ref={ref} {...props}/>
+}
+
 const App = () => {
-  const example = useMyHook()
   return (
     <div>
-      {example}
+      <MySection/>
+      <MySection hue={120} />
+      <MySection hue={240}/>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,20 @@
 import * as React from 'react';
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
-
+export const useMyHook = (ref:any) => {
+  let [revealed, setRevealed] = React.useState<boolean>(false);
   React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
+    let observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setRevealed(true)
+      }
+    });
+    observer.observe(ref.current)
+
     return () => {
-      window.clearInterval(interval);
+      observer.disconnect()
     };
   }, []);
 
-  return counter;
+  return revealed;
 };
 export default useMyHook;
